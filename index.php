2358e3d792a36af5ef4e4f1a2f9e654568c69965
Rikka <mail@rikka.se>
2017-05-10 23:48:49 +0200

    Added EP 2

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="default.css">
+        <title>My Browser Game</title>
+    </head>
+    <body>
+        <h1 class="blueText large">Hello World!</h1>
+        Random Text
+        <h1 class="redText">Hello World again!</h1>
+    </body>
+</html>
